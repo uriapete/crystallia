4af89358cd0efb98857e10767a095bc8328940c9
uriapete <uriarteangelo@gmail.com>
2023-07-01 08:40:00 -0700

    create crystal model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Crystal(models.Model):
+
+    name=models.CharField(max_length=250)
+    mohs_hardness=models.DecimalField(max_digits=3,decimal_places=2)
+    usual_color=models.CharField(max_length=250)
+    img=models.CharField(max_length=5000)
+    finding_place=models.CharField(max_length=9000)
+
+    class Meta:
+        verbose_name = _("crystal")
+        verbose_name_plural = _("crystals")
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("crystal_detail", kwargs={"pk": self.pk})
