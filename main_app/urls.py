aca102520f6aa0a2e175ff3216bc752eedcbb8e1
uriapete <uriarteangelo@gmail.com>
2023-07-01 15:31:44 -0700

    create crystal crud path urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("crystals/",view=views.CrystalListView.as_view(),name="crystal_list"),
+    path("crystals/add/",view=views.CrystalCreateView.as_view(),name="crystal_create"),
+    path("crystals/<int:pk>/",view=views.CrystalDetailView.as_view(),name="crystal_detail"),
+    path("crystals/<int:pk>/update",view=views.CrystalUpdateView.as_view(),name="crystal_update"),
+    path("crystals/<int:pk>/delete",view=views.CrystalDeleteView.as_view(),name="crystal_delete"),
 ]
