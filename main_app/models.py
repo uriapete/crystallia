81ab12e48adfdfbc89063b9dc056ea65de39ebec
uriapete <uriarteangelo@gmail.com>
2023-07-03 15:41:43 -0700

    models - change CrystalType.bio field from CharField to TextField

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,7 @@ class CrystalType(models.Model):
     mohs_hardness=models.DecimalField(max_digits=3,decimal_places=2,blank=True,null=True)
     usual_color=models.CharField(max_length=250,blank=True,null=True)
     img=models.CharField(max_length=5000,blank=True,null=True)
-    bio=models.CharField(max_length=9000,blank=True,null=True)
+    bio=models.TextField(max_length=5000,blank=True,null=True)
     added_on=models.DateTimeField(auto_now_add=True)
 
     class Meta:
