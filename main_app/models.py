676aefd7070c2cea43e9b5bfe913027b57ba0ac6
uriapete <uriarteangelo@gmail.com>
2023-07-03 15:54:05 -0700

    create specific Crystal model

--- main_app/models.py
+++ main_app/models.py
@@ -21,3 +21,23 @@ class CrystalType(models.Model):
 
     def get_absolute_url(self):
         return reverse("crystal_type_detail", kwargs={"pk": self.pk})
+    
+class Crystal(models.Model):
+
+    type=models.ForeignKey(CrystalType,on_delete=models.CASCADE,related_name="crystal",blank=False,null=False)
+    img=models.CharField(max_length=5000,blank=True,null=True)
+    summary=models.TextField(max_length=5000,blank=True,null=True)
+    color=models.CharField(max_length=250,blank=True,null=True)
+    mohs_hardness=models.DecimalField(max_digits=3,decimal_places=2,blank=True,null=True)
+    added_on=models.DateTimeField(auto_now_add=True)
+    found_on=models.DateField(null=True,blank=True)
+
+    class Meta:
+        verbose_name = _("crystal")
+        verbose_name_plural = _("crystals")
+
+    def __str__(self):
+        return f"{self.type.name} {self.pk}"
+
+    def get_absolute_url(self):
+        return reverse("crystal_detail", kwargs={"pk": self.pk})
