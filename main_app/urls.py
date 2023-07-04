fa0a9658df97bc8dd4ccef0b5bae0173c855adb5
uriapete <uriarteangelo@gmail.com>
2023-07-04 12:28:03 -0700

    create crystals urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path("crystal_types/<int:pk>/",view=views.CrystalTypeDetailView.as_view(),name="crystal_type_detail"),
     path("crystal_types/<int:pk>/update",view=views.CrystalTypeUpdateView.as_view(),name="crystal_type_update"),
     path("crystal_types/<int:pk>/delete",view=views.CrystalTypeDeleteView.as_view(),name="crystal_type_delete"),
+    path("crystals/",view=views.CrystalListView.as_view(),name="crystal_list"),
+    path("crystals/add/",view=views.CrystalCreateView.as_view(),name="crystal_create"),
+    path("crystals/<int:pk>/",view=views.CrystalDetailView.as_view(),name="crystal_detail"),
+    path("crystals/<int:pk>/update",view=views.CrystalUpdateView.as_view(),name="crystal_update"),
+    path("crystals/<int:pk>/delete",view=views.CrystalDeleteView.as_view(),name="crystal_delete"),
 ]
