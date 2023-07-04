c2875c532794ed50087e29baf1ecce0bf6887cfb
uriapete <uriarteangelo@gmail.com>
2023-07-04 13:20:18 -0700

    add collection urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path("crystals/<int:pk>/",view=views.CrystalDetailView.as_view(),name="crystal_detail"),
     path("crystals/<int:pk>/update",view=views.CrystalUpdateView.as_view(),name="crystal_update"),
     path("crystals/<int:pk>/delete",view=views.CrystalDeleteView.as_view(),name="crystal_delete"),
+    path("collections/",view=views.CollectionListView.as_view(),name="collection_list"),
+    path("collections/add/",view=views.CollectionCreateView.as_view(),name="collection_create"),
+    path("collections/<int:pk>/",view=views.CollectionDetailView.as_view(),name="collection_detail"),
+    path("collections/<int:pk>/update",view=views.CollectionUpdateView.as_view(),name="collection_update"),
+    path("collections/<int:pk>/delete",view=views.CollectionDeleteView.as_view(),name="collection_delete"),
 ]
