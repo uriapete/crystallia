c19b6058c7f247549b5b6e54533c1e79c288c595
uriapete <uriarteangelo@gmail.com>
2023-07-03 17:33:20 -0700

    settings.py - add whitenoise middleware & allow CORS

--- crystallia/settings.py
+++ crystallia/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -79,6 +80,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'crystallia.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
